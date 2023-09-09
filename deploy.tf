de267ca3c5f7577b009591da5b4ac357bf45d30e
Muhammad Rizwan Ali <37344935+mrizwanali@users.noreply.github.com>
2023-09-09 16:06:28 +0200

    Deployment Files-Step_3

---
+++ deploy.tf
@@ -0,0 +1,41 @@
+provider "kubernetes" {
+  config_path = "/home/rizwan/.kube/config"
+  context    = "cluster-admin@ionos_k8s"
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  #version    = "your-prometheus-version"  # Replace with the desired version
+  namespace   = "monitoring"
+
+  values = [
+    "${file("prometheus.yaml")}"
+  ]
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace   = "monitoring"
+
+  values = [
+    "${file("grafana-values.yaml")}"
+  ]
+}
+
+
+resource "helm_release" "blackbox-exporter" {
+  name       = "blackbox-exporter"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-blackbox-exporter"
+  #version    = "your-prometheus-version"  # Replace with the desired version
+  namespace   = "monitoring"
+
+  values = [
+    "${file("black-box-values.yaml")}"
+  ]
+}
+
