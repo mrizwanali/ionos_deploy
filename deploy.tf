a929ee84626583a44ac6d5b7090f5565c792994f
Muhammad Rizwan Ali <37344935+mrizwanali@users.noreply.github.com>
2023-09-10 11:58:58 +0200

    Added Namespace

--- deploy.tf
+++ deploy.tf
@@ -1,40 +1,51 @@
+# This is a comment in the module
 provider "kubernetes" {
   #config_path = "/home/rizwan/.kube/config"
-  config_path = var.config_path
   context    = "cluster-admin@ionos_kube"
 }
 
+# Define a Kubernetes namespace for monitoring
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+# Define a Helm release for Prometheus
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
-  namespace   = "monitoring"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
+  # Use values from prometheus.yaml file
   values = [
-    "${file("prometheus.yaml")}"
+    file("prometheus.yaml")
   ]
 }
 
+# Define a Helm release for Grafana
 resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
-  namespace   = "monitoring"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
+  # Use values from grafana-values.yaml file
   values = [
-    "${file("grafana-values.yaml")}"
+    file("grafana-values.yaml")
   ]
 }
 
-
+# Define a Helm release for Blackbox Exporter
 resource "helm_release" "blackbox-exporter" {
   name       = "blackbox-exporter"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus-blackbox-exporter"
-  namespace   = "monitoring"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
+  # Use values from black-box-values.yaml file
   values = [
-    "${file("black-box-values.yaml")}"
+    file("black-box-values.yaml")
   ]
 }
-
