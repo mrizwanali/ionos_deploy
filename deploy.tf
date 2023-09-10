85c0d2c9cc96f658045daf4102b4f44c07b6a526
Muhammad Rizwan Ali <37344935+mrizwanali@users.noreply.github.com>
2023-09-10 20:30:43 +0200

    Adjusted few comments

--- deploy.tf
+++ deploy.tf
@@ -1,49 +1,49 @@
-# This is a comment in the module
+# Defining Terraform Kuberenetes Provider
 provider "kubernetes" {
- #context    = "cluster-admin@ionos_kube"
+ # context    = "cluster-admin@ionos_kube" # Already mentioned in config file.
 }
 
-# Define a Kubernetes namespace for monitoring
+# Defining a Kubernetes namespace for monitoring
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
   }
 }
 
-# Define a Helm release for Prometheus
+# Defining a Helm release for Prometheus
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
-  # Use values from prometheus.yaml file
+  # Using values from prometheus.yaml file
   values = [
     file("prometheus.yaml")
   ]
 }
 
-# Define a Helm release for Grafana
+# Defining a Helm release for Grafana
 resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
-  # Use values from grafana-values.yaml file
+  # Using values from grafana-values.yaml file
   values = [
     file("grafana-values.yaml")
   ]
 }
 
-# Define a Helm release for Blackbox Exporter
+# Defining a Helm release for Blackbox Exporter
 resource "helm_release" "blackbox-exporter" {
   name       = "blackbox-exporter"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus-blackbox-exporter"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
-  # Use values from black-box-values.yaml file
+  # Using values from black-box-values.yaml file
   values = [
     file("black-box-values.yaml")
   ]
